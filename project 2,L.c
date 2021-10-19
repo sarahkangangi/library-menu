a0b9b493185e495484e203aad75d811a6dafe405
sarahkangangi <90698814+sarahkangangi@users.noreply.github.com>
2021-10-19 08:43:31 +0300

    Add files via upload

---
+++ project 2,L.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("school library\n");
+    printf("WELCOME Valentine\n");
+    printf("what would you like to do\n");
+    printf("1. add a new book\n");
+    printf("2. borrow another book");
+
+    return 0;
+
+}
